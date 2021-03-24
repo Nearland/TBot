9351c8acac6295410ada1effb54cbc0e04fae15a
k1_245_01 <s>
2021-03-24 20:25:25 +0300

    add commands

--- main.py
+++ main.py
@@ -1,16 +1,90 @@
+import feedparser
 import pyowm
 from pyowm.exceptions import api_response_error, api_call_error
 import telebot
+from selenium import webdriver
+import  urllib.request as u
+from time import sleep
+import requests
+from bs4 import  BeautifulSoup
 
 bot = telebot.TeleBot("1644586994:AAGtW78FVpmDscoiV-ZRWAXNvFLrJ-aKjbo")  # апи бота
 
 owm = pyowm.OWM('797385fc66158e63cb61ac82a7d4ee8c', language="ru")  # ключ и язык
 
+driver = webdriver.Chrome()
 
-# @bot.message_handler(content_types=['text'])  # отвечает на тип текст
-def send_echo(message):  # дублирует
+soup = BeautifulSoup()
+
+@bot.message_handler(commands=['search_in_youtube'])
+def search_youtube(message):
+   messages = bot.send_message(message.chat.id, "Введите текст, который хотите найти на YouTube?")
+   bot.register_next_step_handler(messages, search)  # после вызова команды search_in_youtube идет это команда
+
+
+@bot.message_handler(content_types='text')
+def text(message):
+   pass
+
+
+def search(message):
+   bot.send_message(message.chat.id, "Начинаю поиск")
+   link = "https://www.youtube.com/results?search_query=" + message.text  # ссылка на поиск видео в YouTube
+   r = requests.get(link)
+   soup = BeautifulSoup(r.text, 'html.parser')
+   x = soup.find('a', id('video-title'))
+   print(x.get("div"))
+   # file = u.urlopen(link)
+   # html = file.read()
+
+
+   # driver.get(link)
+   # video = driver.find_elements_by_id("video-title")  # элементы на ютую видео
+   # for i in range(len(video)):
+   #     bot.send_message(message.chat.id, video[i].get_attribute('href'))
+   #     if i == 10:
+   #         break
+
+ # for video in videos:
+ #        bot.send_message(message.chat.id, video.get_attribute('href'))
+
+@bot.message_handler(commands=['news'])
+def news(message):
+    def parseRSS(rss_url):
+        return feedparser.parse(rss_url)
+
+    def getHeadlines(rss_url):
+        headlines = []
+
+        feed = parseRSS(rss_url)
+        for newsitem in feed['items']:
+            # headlines.append(newsitem['title'])
+            headlines.append(newsitem['link'])
+            # headlines.append(newsitem['id'])
+            # headlines.append(newsitem['summary'])
+            # headlines.append(newsitem['published'])
+
+        return headlines
+
+    allheadlines = []
+
+    newsurls = {
+        'googlenews': 'https://news.google.com/rss?hl=ru&gl=RU&ceid=RU:ru',  # сайт с новостями
+    }
+
+    for key, url in newsurls.items():
+        allheadlines.extend(getHeadlines(url))
+
+    for hl in allheadlines:
+        pass
+        bot.send_message(message.chat.id, hl)
+
+
+@bot.message_handler(commands=['weather'])
+def weather(message):  # дублирует
     try:
-        city_name = message.text[9:]
+        city_name = message.text[9:]  # цифра 9 удаляет слово weather
+
         bot.send_message(message.chat.id, "Погода в городе " + city_name)
         observation = owm.weather_at_place(city_name)  # Место где будет показывавть погоду
         w = observation.get_weather()
@@ -21,19 +95,12 @@ def send_echo(message):  # дублирует
         answer += "Ветер " + str(wind) + " м/c" + "\n"
         bot.send_message(message.chat.id, answer)  # ввывод в телеграмм
     except api_response_error.NotFoundError:
-        bot.send_message(message.chat.id, "Неправильный город/страна")
+        bot.send_message(message.chat.id, "Нет данных город/страна " + city_name)
         pass
-
-
-@bot.message_handler(commands=['weather'])
-def helpp(message):
-    bot.send_message(message.chat.id, "Вы перешли в режим просмотра погоды")
-    send_echo(message)
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Начало работы")
+    except api_call_error.APICallError:
+        if city_name == "":
+            bot.send_message(message.chat.id, "Вы не ввели город/страна")
+            pass
 
 
 bot.polling(none_stop=True)
